9f311c77b3cbc95da37987bc35eb91ec113daf11
Bobby <boobeh321@gmail.com>
2026-03-17 20:37:48 -1000

    Use IntersectionObserver API to add class onto elements

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+const revealTargets = document.querySelectorAll(
+    '.collection-header, .collection-card'
+  );
+  
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.12,
+      rootMargin: '0px 0px -40px 0px',
+    }
+  );
+  
+  revealTargets.forEach((el) => observer.observe(el));
